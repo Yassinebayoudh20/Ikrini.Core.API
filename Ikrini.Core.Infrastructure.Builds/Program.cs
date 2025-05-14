8965494e6c82e0fead4e6071c4bb661cf9e99186
Yassine Bayoudh <y.bayoudh@case-tunisia.com>
2025-05-14 13:42:23 +0100

    INFRA : Initialize Build Pipeline

---
+++ Ikrini.Core.Infrastructure.Builds/Program.cs
@@ -0,0 +1,30 @@
+﻿// ---------------------------------------------------------------
+//   Copyright © Yassine Bayoudh. All Rights Reserved. | Ikrini
+// ---------------------------------------------------------------
+
+using System.IO;
+using ADotNet.Clients.Builders;
+using ADotNet.Models.Pipelines.GithubPipelines.DotNets;
+
+string buildScriptPath = "../../../../.github/workflows/dotnet.yml";
+
+string directoryPath = Path.GetDirectoryName(buildScriptPath);
+
+if (Directory.Exists(directoryPath) is false)
+{
+    Directory.CreateDirectory(directoryPath);
+}
+
+GitHubPipelineBuilder.CreateNewPipeline()
+    .SetName("Ikrini Core API Build")
+        .OnPush("master")
+        .OnPullRequest("master")
+            .AddJob("build", job => job
+                  .WithName("Build")
+                  .RunsOn(BuildMachines.Windows2022)
+                  .AddCheckoutStep("Check Out")
+                  .AddSetupDotNetStep(version: "8.0.16")
+                  .AddRestoreStep()
+                  .AddBuildStep()
+                  .AddTestStep(command: "dotnet test --no-build --verbosity normal --filter FullyQualifiedName!~Integrations"))
+.SaveToFile(buildScriptPath);
