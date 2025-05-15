c418bae6130f2a64393bccafbb9f288115a958f8
Yassine Bayoudh <y.bayoudh@case-tunisia.com>
2025-05-15 15:12:24 +0100

    ShouldReturnOkWithAllCarsAsync -> FAIL

---
+++ Ikrini.Core.API/Controllers/CarsController.cs
@@ -0,0 +1,27 @@
+﻿using Ikrini.Core.API.Models.Cars;
+using Ikrini.Core.API.Services.Foundations.Cars;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using RESTFulSense.Controllers;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Ikrini.Core.API.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    internal class CarsController : RESTFulController
+    {
+        private readonly ICarService carService;
+
+        public CarsController(ICarService carService)
+        {
+            this.carService = carService;
+        }
+
+        [HttpGet]
+        public async ValueTask<ActionResult<IQueryable<Car>>> GetAllCarsAsync() =>
+            NotImplemented("Endpoint not implmented");
+
+    }
+}
