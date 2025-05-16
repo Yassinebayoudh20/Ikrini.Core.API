4c848207597c58b60296fff1fb4c3f9e8224468b
Yassine Bayoudh <y.bayoudh@case-tunisia.com>
2025-05-16 11:59:07 +0100

    ShouldThrowCriticalDependencyExceptionOnAddIfSqlErrorOccurredAndLogItAsync -> FAIL

---
+++ Ikrini.Core.API.Tests.Units/Services/Foundations/Cars/CarServiceTests.Exceptions.Add.cs
@@ -0,0 +1,70 @@
+﻿// ---------------------------------------------------------------
+//   Copyright © Yassine Bayoudh. All Rights Reserved. | Ikrini
+// ---------------------------------------------------------------
+
+using FluentAssertions;
+using Ikrini.Core.API.Models.Cars;
+using Ikrini.Core.API.Models.Cars.Exceptions;
+using Microsoft.Data.SqlClient;
+using Moq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ikrini.Core.API.Tests.Units.Services.Foundations.Cars
+{
+    public partial class CarServiceTests
+    {
+        [Fact]
+        public async Task ShouldThrowCriticalDependencyExceptionOnAddIfSqlErrorOccurredAndLogItAsync()
+        {
+            //Arrange
+            Car randomCar = CreateRandomCar();
+            SqlException sqlException = CreateSqlException();
+
+            var failedCarStorageException =
+               new FailedCarStorageException(
+                   message: "Failed Car storage occurred , contact support.",
+                   innerException: sqlException);
+
+            var expectedCarDependencyException =
+                new CarDependencyException(
+                    message: "Car dependency error occurred, contact support.",
+                    innerException: failedCarStorageException);
+
+            this.storageBrokerMock.Setup(broker =>
+                broker.InsertCarAsync(randomCar))
+                    .ThrowsAsync(sqlException);
+            //Act
+
+            ValueTask<Car> addCarTask =
+                this.carService.AddCarAsync(randomCar);
+
+            CarDependencyException actualCarDependencyException = 
+                await Assert.ThrowsAsync<CarDependencyException>(testCode : addCarTask.AsTask);
+
+            //Assert
+
+            actualCarDependencyException.Should().BeEquivalentTo(expectedCarDependencyException);
+
+            //this.datetimeBrokerMock.Verify(broker =>
+            //    broker.GetCurrentDateTimeOffsetAsync(),
+            //        Times.Once);
+
+            this.loggingBrokerMock.Verify(broker =>
+                broker.LogCriticalAsync(It.Is(SameExceptionAs(
+                    expectedCarDependencyException))),
+                        Times.Once);
+
+            this.storageBrokerMock.Verify(broker =>
+                broker.InsertCarAsync(randomCar),
+                    Times.Never);
+
+            this.datetimeBrokerMock.VerifyNoOtherCalls();
+            this.loggingBrokerMock.VerifyNoOtherCalls();
+            this.storageBrokerMock.VerifyNoOtherCalls();
+        }
+    }
+}
--- Ikrini.Core.API/Services/Foundations/Cars/CarService.Exceptions.cs
+++ Ikrini.Core.API/Services/Foundations/Cars/CarService.Exceptions.cs
@@ -1,6 +1,4 @@
-﻿using Ikrini.Core.API.Brokers.Loggings;
-using Ikrini.Core.API.Brokers.Storages;
-using Ikrini.Core.API.Models.Cars;
+﻿using Ikrini.Core.API.Models.Cars;
 using Ikrini.Core.API.Models.Cars.Exceptions;
 using Microsoft.Data.SqlClient;
 using System;
@@ -12,14 +10,28 @@ namespace Ikrini.Core.API.Services.Foundations.Cars
 {
     public partial class CarService : ICarService
     {
+        private delegate ValueTask<Car> ReturningCarFunction();
+
         private delegate ValueTask<IQueryable<Car>> ReturningCarsFunction();
 
-        private async ValueTask<IQueryable<Car>> TryCatch(
-            ReturningCarsFunction returningCarsFunction)
+        private async ValueTask<Car> TryCatch(
+            ReturningCarFunction returningCarFunction)
         {
             try
             {
-                 return await returningCarsFunction();
+                return await returningCarFunction();
+            }
+            catch (NullCarException nullCarException)
+            {
+                throw await CreateAndLogValidationExceptionAsync(nullCarException);
+            }
+            catch (InvalidCarException invalidCarException)
+            {
+                throw await CreateAndLogValidationExceptionAsync(invalidCarException);
+            }
+            catch (NotFoundCarException notFoundCarException)
+            {
+                throw await CreateAndLogValidationExceptionAsync(notFoundCarException);
             }
             catch (SqlException sqlException)
             {
@@ -30,7 +42,7 @@ namespace Ikrini.Core.API.Services.Foundations.Cars
 
                 throw await CreateAndLogDependencyExceptionAsync(failedCarStorageException);
             }
-            catch(Exception exception)
+            catch (Exception exception)
             {
                 var failedCarServiceException =
                     new FailedCarServiceException(
@@ -38,6 +50,44 @@ namespace Ikrini.Core.API.Services.Foundations.Cars
                         innerException: exception);
                 throw await CreateAndLogServiceExceptionAsync(failedCarServiceException);
             }
+        }
+
+        private async ValueTask<IQueryable<Car>> TryCatch(
+            ReturningCarsFunction returningCarsFunction)
+        {
+            try
+            {
+                return await returningCarsFunction();
+            }
+            catch (SqlException sqlException)
+            {
+                var failedCarStorageException =
+                    new FailedCarStorageException(
+                        message: "Failed Car storage occurred , contact support.",
+                        innerException: sqlException);
+
+                throw await CreateAndLogDependencyExceptionAsync(failedCarStorageException);
+            }
+            catch (Exception exception)
+            {
+                var failedCarServiceException =
+                    new FailedCarServiceException(
+                        message: "Failed Car service occurred, contact support.",
+                        innerException: exception);
+                throw await CreateAndLogServiceExceptionAsync(failedCarServiceException);
+            }
+        }
+
+        private async ValueTask<CarValidationException> CreateAndLogValidationExceptionAsync(Xeption exception)
+        {
+            var carValidationException =
+                new CarValidationException(
+                    message: "Car validation error occurred, fix the errors and try again.",
+                    innerException: exception);
+
+            await this.loggingBroker.LogErrorAsync(carValidationException);
+
+            return carValidationException;
         }
 
         private async ValueTask<CarDependencyException> CreateAndLogDependencyExceptionAsync(Xeption exception)
