df0ec7f5337ed228506dd55004598440c098d6d3
Yassine Bayoudh <y.bayoudh@case-tunisia.com>
2025-05-19 14:38:24 +0100

    CONTROLLERS : Car Add

--- Ikrini.Core.API/Controllers/CarsController.cs
+++ Ikrini.Core.API/Controllers/CarsController.cs
@@ -1,10 +1,8 @@
 ﻿using Ikrini.Core.API.Models.Foundations.Cars;
 using Ikrini.Core.API.Models.Foundations.Cars.Exceptions;
 using Ikrini.Core.API.Services.Foundations.Cars;
-using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using RESTFulSense.Controllers;
-using System;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -16,20 +14,50 @@ namespace Ikrini.Core.API.Controllers
     {
         private readonly ICarService carService;
 
-        public CarsController(ICarService carService)
-        {
+        public CarsController(ICarService carService) =>
             this.carService = carService;
-        }
+
 
         [HttpGet]
         public async ValueTask<ActionResult<IQueryable<Car>>> GetAllCarsAsync()
         {
-            try 
+            try
             {
                 IQueryable<Car> retievedCars =
                  await this.carService.RetrieveAllCarsAsync();
 
                 return Ok(retievedCars);
+            }
+            catch (CarDependencyException carDependencyException)
+            {
+                return InternalServerError(carDependencyException);
+            }
+            catch (CarServiceException carServiceException)
+            {
+                return InternalServerError(carServiceException);
+            }
+        }
+
+        [HttpPost]
+        public async ValueTask<ActionResult<Car>> PostCarAsync(Car car)
+        {
+            try
+            {
+                Car addedCar = await this.carService.AddCarAsync(car);
+                return Created(addedCar);
+            }
+            catch (CarValidationException carValidationException)
+            {
+                return BadRequest(carValidationException.InnerException);
+            }
+            catch (CarDependencyValidationException carDependencyValidationException)
+                when (carDependencyValidationException.InnerException is AlreadyExistsCarException)
+            {
+                return Conflict(carDependencyValidationException.InnerException);
+            }
+            catch (CarDependencyValidationException carDependencyValidationException)
+            {
+                return BadRequest(carDependencyValidationException.InnerException);
             }
             catch (CarDependencyException carDependencyException)
             {
